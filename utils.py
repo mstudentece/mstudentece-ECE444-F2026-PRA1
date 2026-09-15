0975c7e3486905afb8100222a3e2bd5c418d55d4
mstudentece <153149388+mstudentece@users.noreply.github.com>
2026-09-15 03:42:33 +0000

    fixed str input issue in utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
 class utils:
     def reversed(self, number: int) -> int:
+        if type(number) is not int:
+            raise TypeError("input must be int")
         if number < 0:
             return -int(str(abs(number))[::-1])
         return int(str(number)[::-1])
 
     def formatter(self, number: int):
+        if type(number) is not int:
+            raise TypeError("input must be int")
         return bin(number), oct(number)
-        
